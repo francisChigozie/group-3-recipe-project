96bf6ca46f215c079b4cc9d8837cffd41ac5785e
chigozieFrancis <francisc@hs-pforzheim.de>
2021-04-21 21:02:55 +0200

    add initial files

---
+++ group-3-project.js
@@ -0,0 +1,105 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+  function myFunctiontwo() {
+    var dots = document.getElementById("dotstwo");
+    var moreText = document.getElementById("moretwo");
+    var btnText = document.getElementById("myBtntwo");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+  function myFunctionthree() {
+    var dots = document.getElementById("dotsthree");
+    var moreText = document.getElementById("morethree");
+    var btnText = document.getElementById("myBtnthree");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctionfour() {
+    var dots = document.getElementById("dotsfour");
+    var moreText = document.getElementById("morefour");
+    var btnText = document.getElementById("myBtnfour");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  
+  function myFunctionfive() {
+    var dots = document.getElementById("dotsfive");
+    var moreText = document.getElementById("morefive");
+    var btnText = document.getElementById("myBtnfive");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunctionsix() {
+    var dots = document.getElementById("dotssix");
+    var moreText = document.getElementById("moresix");
+    var btnText = document.getElementById("myBtnsix");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+
+
+
+
+function myFunc() {
+    alert("No share for you. Go to hugry jail!");
+  }
